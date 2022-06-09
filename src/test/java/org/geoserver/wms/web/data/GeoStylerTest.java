17d6df0874729b048fd4b1f1adf7bdf6b3692ef9
Daniel Koch <koch@terrestris.de>
2022-06-09 16:37:10 +0200

    Add test for successful render of the GeoStyler in the Style page

---
+++ src/test/java/org/geoserver/wms/web/data/GeoStylerTest.java
@@ -0,0 +1,41 @@
+package org.geoserver.wms.web.data;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.wicket.util.file.File;
+import org.apache.wicket.util.tester.FormTester;
+import org.geoserver.web.GeoServerWicketTestSupport;
+import org.junit.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class GeoStylerTest extends GeoServerWicketTestSupport {
+
+    @Test
+    public void rendersTheStylePageIncludingTheGeoStyler() throws IOException, URISyntaxException {
+        login();
+        tester.startPage(StyleNewPage.class);
+
+        FormTester form = tester.newFormTester("styleForm");
+
+        File styleFile =
+                new File(
+                        new java.io.File(
+                                getClass()
+                                        .getClassLoader()
+                                        .getResource(
+                                                "default_point.sld")
+                                        .toURI()));
+        String sld =
+                IOUtils.toString(new FileReader(styleFile))
+                        .replaceAll("\r\n", "\n")
+                        .replaceAll("\r", "\n");
+        form.setValue("context:panel:name", "default_point_style");
+        form.setValue("styleEditor:editorContainer:editorParent:editor", sld);
+        form.submit();
+
+        tester.assertComponent("styleForm:geoStyler", GeoStyler.class);
+        tester.assertRenderedPage(StyleNewPage.class);
+    }
+}
